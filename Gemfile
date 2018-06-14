b67a73723a1911f5d09db973d9bd2618f49457c2
ashvin14 <yashkhrnr@gmail.com>
2018-06-14 15:42:11 +0530

    udpated gem file

--- Gemfile
+++ Gemfile
@@ -27,10 +27,13 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'devise'
 
+gem 'pg','~> 0.18'
+
+
 gem 'simple_form'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-
+  
 # Use Unicorn as the app server
 # gem 'unicorn'
 # Use Capistrano for deployment
@@ -44,7 +47,6 @@ gem 'mini_magick', '~> 4.3'
 
 gem 'http'
 group :development, :test do
-  gem 'sqlite3'
 
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
@@ -61,7 +63,6 @@ group :development do
 end
 
 group :production do
-  gem 'pg'
   gem 'rails_12factor'
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20180612111247) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "images", force: :cascade do |t|
     t.string   "url"
     t.integer  "user_id"
@@ -20,7 +23,7 @@ ActiveRecord::Schema.define(version: 20180612111247) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "images", ["user_id"], name: "index_images_on_user_id"
+  add_index "images", ["user_id"], name: "index_images_on_user_id", using: :btree
 
   create_table "stores", force: :cascade do |t|
     t.integer  "Image_id"
@@ -29,8 +32,8 @@ ActiveRecord::Schema.define(version: 20180612111247) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "stores", ["Image_id"], name: "index_stores_on_Image_id"
-  add_index "stores", ["user_id"], name: "index_stores_on_user_id"
+  add_index "stores", ["Image_id"], name: "index_stores_on_Image_id", using: :btree
+  add_index "stores", ["user_id"], name: "index_stores_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: ""
@@ -49,7 +52,7 @@ ActiveRecord::Schema.define(version: 20180612111247) do
     t.string   "provider"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
